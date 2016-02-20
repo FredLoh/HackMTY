3ec18e33d755418877d055b5f5fc76315d61aa3c
Frederik Lohner <freddylohner@gmail.com>
2016-02-20 17:19:17 -0600

    Amazon Mongo

--- BackendApp/BackendApp/models/db.js
+++ BackendApp/BackendApp/models/db.js
@@ -3,3 +3,6 @@
  */
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/RentaCanchas');
+//mongoose.connect('mongodb://fredloh:fredloh123@ec2-52-36-51-12.us-west-2.compute.amazonaws.com:27017')
+//mongo ec2-52-36-51-12.us-west-2.compute.amazonaws.com/RentaCanchas -u fredloh -p fredloh123
+//'mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB'
