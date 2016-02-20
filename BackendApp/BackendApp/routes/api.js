f9136552047bd26a6bfc9b12bba0220b6495dbc4
Frederik Lohner <freddylohner@gmail.com>
2016-02-20 16:13:24 -0600

    Commit to push to EC2

--- BackendApp/BackendApp/routes/api.js
+++ BackendApp/BackendApp/routes/api.js
@@ -7,19 +7,19 @@ var canchas = mongoose.model('cancha');
 /* GET home page. */
 router.get('/cancha', function(req, res, next) {
     var query = require('url').parse(req.url,true).query;
-    if (query.longitude === "" || query.latitude === "" || query.longitude === undefined || query.latitude === undefined) {
+    if (query.long === "" || query.lat === "" || query.long === undefined || query.lat === undefined) {
         res.sendStatus(404).end();
     } else {
         var limit = req.query.limit || 10;
         // get the max distance or set it to 8 kilometers
-        var maxDistance = req.query.distance || 8;
+        var maxDistance = req.query.dist || 8;
 
         // we need to convert the distance to radians
         // the raduis of Earth is approximately 6371 kilometers
         maxDistance /= 6371;
         var coords = [];
-        coords[0] = req.query.longitude || 0;
-        coords[1] = req.query.latitude || 0;
+        coords[0] = req.query.long || 0;
+        coords[1] = req.query.lat || 0;
         var found = canchas.find({
             location: {
                 $near: coords,
