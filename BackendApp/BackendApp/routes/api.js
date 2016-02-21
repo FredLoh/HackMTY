b4c20d1e70ba5f23dcf3cf3ccfcbd5244ef1a617
Frederik Lohner <freddylohner@gmail.com>
2016-02-20 22:39:45 -0600

    max distance change

--- BackendApp/BackendApp/routes/api.js
+++ BackendApp/BackendApp/routes/api.js
@@ -1,13 +1,36 @@
 var express = require('express');
+var http = require('http');
+var https = require("https");
 var router = express.Router();
 var url = require('url');
 var mongoose = require('mongoose');
 var canchas = mongoose.model('cancha');
 
+
+var options = {
+    host: 'localhost',
+    path: '/api/canchas'
+};
+
+callback = function(response) {
+    var str = '';
+    console.log("GOT HERE LADS");
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+        str += chunk;
+    });
+
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+        console.log(str);
+
+    });
+};
+
 /* GET home page. */
 router.get('/cancha', function(req, res, next) {
     var query = require('url').parse(req.url,true).query;
-    console.log(query);
+    //console.log(query);
     if (query.long === "" || query.lat === "" || query.long === undefined || query.lat === undefined || query.day === "" || query.day === undefined
         || query.month === "" || query.month === undefined || query.year === "" || query.year === undefined || query.hor === "" || query.hor === undefined
         || query.minu === "" || query.minu === undefined || query.dur === "" || query.dur === undefined ) {
@@ -20,14 +43,16 @@ router.get('/cancha', function(req, res, next) {
         res.sendStatus(404).end();
     } else if (query.year < 2016 || query.year > 2020) {
         res.sendStatus(404).end();
-    } else if (query.hor < 1 || query.hor > 24) {
+    } else if (query.hor < 0 || query.hor > 24) {
         res.sendStatus(404).end();
     } else if (query.minu < 0 || query.minu > 59) {
         res.sendStatus(404).end();
     } else {
-        var limit = req.query.limit || 10;
+        var limit = req.query.limit || 100;
         // get the max distance or set it to 8 kilometers
-        var maxDistance = req.query.dist || 20;
+        var maxDistance = req.query.dist || 100;
+
+        http.request(options, callback).end();
 
         // we need to convert the distance to radians
         // the raduis of Earth is approximately 6371 kilometers
